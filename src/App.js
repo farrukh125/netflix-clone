8f1c356adda4b3790411b6898b93ed451a23d040
Farrukh Mehboob <36042782+farrukh125@users.noreply.github.com>
2021-10-12 21:00:52 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
 import Row from "./Row";
 import requests from "./requests";
+import Banner from "./Banner";
 
 function App() {
   return (
     <div className="App">
+      <Banner />
       <Row
         title="NETFLIX ORIGINALS"
         fetchUrl={requests.fetchNetflixOriginals}
-        isLargeRow
+        isLargeRow={true}
       />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
       <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
---
+++ src/Banner.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
+import requests from "./requests";
+
+function Banner() {
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNetflixOriginals);
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+    }
+
+    fetchData();
+  }, []);
+
+  console.log(movie);
+
+  return <header></header>;
+}
+
+export default Banner;
