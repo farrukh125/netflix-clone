9faa63e640bb1b508491ef0a18bee5eb12d56ff1
Farrukh Mehboob <36042782+farrukh125@users.noreply.github.com>
2021-10-15 15:24:11 +0500

    commit

--- src/Nav.js
+++ src/Nav.js
@@ -1,25 +1,27 @@
-import React { useState, useEffect } from 'react'
-import "./Nav.css"
+import React, { useState, useEffect } from "react";
+import "./Nav.css";
+import "./netflix-logo.svg";
 
 function Nav() {
-    useEffect(() => {
-        window.addEventListener("scroll",()=>{
-            if(window.scrollY>100){
-                handleShow(true);
-            }else handleShow(false);
-        });
-        return () => {
-            window.removeEventListener("scroll");
-        };
-    }, []);
+  const [show, handleShow] = useState(false);
 
-    return (
-        <div className={`nav ${show && "nav__black"}`}>
-            <img className="nav__logo" src="https://upload.wikimedia.org/wikipedia/commons/0" alt="Netflix Logo">
-            <img className="nav__avatar" src="https://pbs.twimg.com/profile_images/124-11999041155" alt="Netflix Logo">
-            
-        </div>
-    )
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <img className="nav__logo" src="netflix-logo.svg" alt="Netflix Logo" />
+      <img className="nav__avatar" src="/" alt="Netflix Avatar" />
+    </div>
+  );
 }
 
-export default Nav
+export default Nav;
--- src/Row.js
+++ src/Row.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react";
 import axios from "./axios";
 import "./Row.css";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
   // A snippet of code which runs based on a specific condition/variable
 
@@ -19,7 +22,29 @@ function Row({ title, fetchUrl, isLargeRow }) {
     }
     fetchData();
   }, [fetchUrl]);
-  console.log(movies);
+  //console.log(movies);
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || movie?.title || movie?.original_name || "")
+        .then((url) => {
+          // https://www.youtube.com/watch?v=htqXL94Rza4
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => console.error(error));
+    }
+  };
 
   return (
     <div className="row">
@@ -29,6 +54,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
         {movies.map((movie) => (
           <img
             key={movie.id}
+            onClick={() => handleClick(movie)}
             className={`row__poster ${isLargeRow && "row__posterLarge"}`}
             src={`${base_url}${
               isLargeRow ? movie.poster_path : movie.backdrop_path
@@ -37,6 +63,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
           />
         ))}
       </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
       {/* container -> posters */}
     </div>
   );
